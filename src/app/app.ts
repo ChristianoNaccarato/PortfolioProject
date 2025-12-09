9a171ae197f6b2799f3adca9d96d574b5a7df00a
Christiano <CN52@myscc.ca>
2025-12-08 21:39:14 -0500

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgOptimizedImage],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('ProtfolioProject');
+}
